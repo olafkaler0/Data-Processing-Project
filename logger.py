35c94ceceab219dc9a9cbeb9f3768e36ab8ea20c
olafkaler0 <olafkaler0@gmail.com>
2024-06-30 13:44:53 +0300

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,16 @@
+# logger.py
+
+import logging
+
+def setup_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    handler = logging.FileHandler(f'{name}.log')
+    handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
